b17eb7f9c2577e5c64e76b54ef3451e86faef36b
Maiyuren <maiyuren51@gmail.com>
2020-02-21 06:21:29 +1100

    update

--- proj.py
+++ proj.py
@@ -2,14 +2,21 @@
 
 class Library:
 
-    def __init__(self):
+    def __init__(self, lib_id):
         self.books = []
         self.sign_up_preocess = None
         self.max_ship_num = None
         self.num_books = None
+        self.lib_id = lib_id
 
-    def remove_book(self, id):
-        del self.books[]
+        # 0 for not-activated, -1 for activating, 1 for activated
+        self.flag = 0
+        self.num_activating =
+
+    def remove_book(self, scores, id):
+        # Todo Make an exception if the book not in this library
+        self.books.remove([id, scores[id]])
+        self.num_books -= 1
 
     def load_library(self, scores, info_line, book_line):
 
@@ -20,3 +27,34 @@ class Library:
         for id in book_line:
             self.books.append([id, scores[id]])
 
+        self.sort_books()
+
+    def sort_books(self):
+        self.books = sorted(self.books, key=lambda l: l[1])
+
+    def get_book(self):
+        """ This returns [id, value] for the best book """
+        return self.books[0]
+
+    def iterate_day(self):
+        if self.flag == -1:
+
+
+def load_data(filename):
+
+    """ Return info which is list of [num_books, num_libraries, num_days]"""
+
+    libs = []
+
+    with open(filename, 'r') as f:
+        info = [int(i) for i in f.readline().split(' ')]
+        scores = [int(i) for i in f.readline().split(' ')]
+
+        for lib in range(info[1]):
+            info_line = [int(i) for i in f.readline().split(' ')]
+            book_line = [int(i) for i in f.readline().split(' ')]
+            library = Library(lib)
+            library.load_library(scores, info_line, book_line)
+            libs.append(library)
+
+    return info, libs
